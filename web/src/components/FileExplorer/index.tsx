57d7119af6819d968c376b111fd1dda3b3a811ba
Wesley <77447630+BurkeDesigns@users.noreply.github.com>
2024-11-24 16:02:56 -0500

    update

--- web/src/components/FileExplorer/index.tsx
+++ web/src/components/FileExplorer/index.tsx
@@ -27,16 +27,18 @@ const component = (props: any) => {
       <div className="folders">
         <List el="column xxsm" className="folder" title="Logos">
           <Icon name="folder" />
-          <Text color="white" style={{whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', width: '100%'}}>Logos</Text>
+          <Text color="currentColor" style={{whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', width: '100%'}}>Logos</Text>
         </List>
         <List el="column xxsm" className="folder" title="Documents">
           <Icon name="folder" />
-          <Text color="white" style={{whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', width: '100%'}}>Documents</Text>
+          <Text color="currentColor" style={{whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', width: '100%'}}>Documents</Text>
         </List>
       </div>
-      {/* <div className="files">
-        <div className="file">file</div>
-      </div> */}
+      <div className="files">
+        <div className="file">
+          <img src="/images/esports@400.jpg" alt="esports@400.jpg" title="esports@400.jpg" />
+        </div>
+      </div>
     </div>
 		</>
 	);
